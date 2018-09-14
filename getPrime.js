384d43a8cf588cfea1aea57ca2e05befe8ea0c0b
sqzrmhj <40202126+sqzrmhj@users.noreply.github.com>
2018-09-15 02:09:45 +0800

    Add files via upload

--- getPrime.js
+++ getPrime.js
@@ -1,5 +1,4 @@
-//function get(d){console.time("sort");for(var a=[],b=0,c=2;c<=d;c++)a.push(c);for(c=1;a[b]*a[b]<d;c++)a[b]*a[b]>a[c]||(0==a[c]%a[b]&&(a.splice(c,1),c--),c==a.length-1&&(b++,c=b));console.log(a);console.log("Total: "+sum(a));console.timeEnd("sort")}function sum(d){for(var a=0,b=0;b<d.length;b++)a+=d[b];return a};function get(d){console.time("sort");for(var a=[],b=0,c=2;c<=d;c++)a.push(c);for(c=1;a[b]*a[b]<d;c++)a[b]*a[b]>a[c]||(0==a[c]%a[b]&&(a.splice(c,1),c--),c==a.length-1&&(b++,c=b));console.log(a);console.log("Total: "+sum(a));console.timeEnd("sort")}function sum(d){for(var a=0,b=0;b<d.length;b++)a+=d[b];return a};
-function get(e){
+function get(e,num){
 	console.time("sort");
 	let res=[],n=0;
 	//console.time("intoarr");
@@ -8,29 +7,37 @@ function get(e){
 	}
 	//console.timeEnd("intoarr");
 	for(let i=1;res[n]*res[n]<e;i++){
-		if(res[n]*res[n]>res[i])continue;
+		//if(res[n]*res[n]>res[i])continue;
 		if(res[i]%res[n]==0){res.splice(i,1);i--;}
 		if(i==res.length-1){n++;i=n;}
 		//else{console.log(i)};
 	}
-	console.log(res);
-	console.log("Total: "+sum(res));
+	for(let i=0;i<res.length;i++){
+		console.log(res[i]);
+	}
+	let total=0;
+	for(let i=0;i<res.length;i++){
+		total+=res[i];
+	}
+	console.log("Amount: "+res.length);
+	console.log("Total: "+total);
+	
+	if (num){
+		console.log("\nOthers:");
+		if(!isNaN(num)&&num<res.length&&num>1&&Math.round(num)==num){
+			console.log("The "+num+"th prime number is: "+res[num-1]);
+			let order=res.indexOf(num);
+			if(order==-1){
+				console.log(num+" is not a prime number in the range.\n");
+			}else{
+				console.log("and "+num+" is the "+(order+1)+"th prime number.\n");
+			}
+		}
+		else{console.log("The parameter is invalid.\n");}
+	}
 	console.timeEnd("sort");
  }
-function sum(arr){
-	let s=0;
-	for(let i=0;i<arr.length;i++){
-		s+=arr[i];
-	}
-	return s;
-}
-get(100000);
-
-
-
-
-
-//function prime(d){console.time("sort");var c=[],a,b;for(a=2;a<=d;a++){for(b=2;b<a&&0!=a%b;b++);a==b&&c.push(a)}console.log(c);console.timeEnd("sort")};
+ 
 function prime(d) {
 	console.time("sort");
 	var c = [], a, b;
@@ -42,4 +49,4 @@ function prime(d) {
 	console.log(c);
 	console.timeEnd("sort");
 }
-console.log(prime(100));
+//console.log(prime(100));
